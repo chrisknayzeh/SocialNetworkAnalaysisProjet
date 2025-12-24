7c82987f692ecbfdc6326c0b29dd9c1457633913
Christian Knayzeh <chris.knayzeh01@gmail.com>
2025-12-24 20:31:15 +0100

    Added utils.py that contains helper functions

---
+++ scripts/utils.py
@@ -0,0 +1,43 @@
+# Generate graph statistics
+
+def graph_stats(graph):
+  import igraph as ig
+  import numpy as np
+
+  num_nodes = graph.vcount()
+  num_edges = graph.ecount()
+
+  num_agents = sum(graph.vs['is_agent'])
+  num_humans = num_nodes - num_agents
+
+  in_degrees = graph.indegree()
+  out_degrees = graph.outdegree()
+
+  q1_in = round(np.percentile(in_degrees,25), 2)
+  q3_in = round(np.percentile(in_degrees,75),2)
+
+  q1_out = round(np.percentile(out_degrees,25), 2)
+  q3_out = round(np.percentile(out_degrees,75),2)
+
+  print("===== GRAPH STATISTICS =====")
+  print(f"Nodes: {num_nodes}")
+  print(f"Edges (unique interactions): {num_edges}")
+  print(f"Humans: {num_humans}")
+  print(f"Agents: {num_agents}")
+
+  print("\nIn-degree statistics:")
+  print("  Mean:", round(np.mean(in_degrees), 2))
+  print(f"  Q1:{q1_in}")
+  print("  Median:", np.median(in_degrees))
+  print(f"  Q3:{q3_in}" )
+  print("  Max:", max(in_degrees))
+
+  print("\nOut-degree statistics:")
+  print("  Mean:", round(np.mean(out_degrees), 2))
+  print(f"  Q1:{q1_out}")
+  print("  Median:", np.median(out_degrees))
+  print(f"  Q3:{q3_out}" )
+  print("  Max:", max(out_degrees))
+
+ 
+
